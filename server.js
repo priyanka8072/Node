7db2e71c9bf3e684a64c9811f19563b6bc421ced
Priyanka Sharma <priyanka.8072@gmail.com>
2017-04-26 16:08:43 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const PORT = 80;
+
+app.get('/', function (req, res) {
+  res.send('Welcome to the portal\n');
+});
+
+
+app.get('/customer', function (req, res) {
+   fs.readFile( __dirname + "/" + "customer.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+app.get('/customer/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "customer.json", 'utf8', function (err, data) {
+       users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+   });
+})
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
